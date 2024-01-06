bcdb94687e5ffefcbee28be8c0f218acd5e6460a
Mannaro <max_ximus@mail.ru>
2024-01-06 20:38:23 +0500

    add new task to Repo

---
+++ LearningTasks/Tasks/src/Task4.java
@@ -0,0 +1,11 @@
+//Вывести на экран слово “JAVA”, в строку, чтобы оно повторилось 10 раз, используя цикл for.
+//Вывод в консоль должен быть таким:
+//JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA
+
+public class Task4 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.print("JAVA ");
+        }
+    }
+}
