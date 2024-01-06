0551164dc3a5ebc2f960d9c50724303bda911875
Mannaro <max_ximus@mail.ru>
2024-01-06 20:57:58 +0500

    simple add new task..

---
+++ LearningTasks/Tasks/src/Task5.java
@@ -0,0 +1,10 @@
+//Вывести на экран слово “JAVA”, в столбик, чтобы оно повторилось 10 раз,
+//используя цикл на ваше усмотрение (for или while).
+
+public class Task5 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("JAVA");
+        }
+    }
+}
